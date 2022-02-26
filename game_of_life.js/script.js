b68a84ca888e07a5708c90caef75ffa8e356f9ac
TomAbrhamyan <torgom.a.g@tumo.org>
2022-02-26 21:15:12 +0400

    add new directory, new functions, add seasons ...

--- game_of_life.js/script.js
+++ game_of_life.js/script.js
@@ -1,8 +1,5 @@
 var socket = io();
-
-side = 15;
-
-
+side = 20;
 
 function setup() {
     frameRate(5);
@@ -11,25 +8,90 @@ function setup() {
 }
 
 
+
+weath = 'summer';
+function winter() {
+    weath = 'winter';
+
+}
+function spring() {
+    weath = 'spring';
+  
+}
+function summer() {
+    weath = 'summer';
+    
+}
+function autumn() {
+    weath = 'autumn';
+
+}
+
+
+
 function nkarel(matrix) {
 
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
+            var toBot = side - side * 0.3
+            textSize(toBot);
             if (matrix[y][x] == 1) {
-               fill("green");
+                fill("green");
+                if (weath == "winter") {
+                    fill("white");
+                    rect(x * side, y * side, side, side);
+                     text('🌿', x * side, y * side + toBot);
+                    setInterval(() => { weath = "spring" }, 80000);
+                } else if (weath == "spring") {
+                    fill("yellowgreen");
+                    rect(x * side, y * side, side, side);
+                text('🌿', x * side, y * side + toBot);
+                    setInterval(() => { weath = "summer" }, 80000);
+                } else if (weath == "summer") {
+                    fill("green");
+                    rect(x * side, y * side, side, side);
+                    text('🌿', x * side, y * side + toBot);
+                    setInterval(() => { weath = "autumn" }, 80000);
+                } else if (weath == "autumn") {
+                    fill("#FED32A");
+                    rect(x * side, y * side, side, side);
+                    text('🌾', x * side, y * side + toBot);
+                    setInterval(() => { weath = "winter" }, 80000);
+                }
+                
             }
             else if (matrix[y][x] == 0) {
-                fill("#acacac");
+                if (weath == 'winter'){
+                    fill("#00f5e4");
+                }
+                else if(weath == 'spring'){
+                    fill("#fff799");
+                }
+                else if(weath == 'summer'){
+                    fill("#FCF6B1");
+                }
+                else if(weath == 'autumn'){
+                    fill("#fffaba");
+                }
+                rect(x * side, y * side, side, side);
+            
             } else if (matrix[y][x] == 2) {
                 fill("yellow");
+                rect(x * side, y * side, side, side);
+                text('🐇', x * side, y * side + toBot);
             } else if (matrix[y][x] == 3) {
                 fill("orange");
+                rect(x * side, y * side, side, side);
+                text("🐆", x * side, y * side + toBot);
             } else if (matrix[y][x] == 4) {
                 fill("red");
+                rect(x * side, y * side, side, side);
             } else if (matrix[y][x] == 5) {
                 fill("black");
+                rect(x * side, y * side, side, side);
+                text('👾', x * side, y * side + toBot);
             }
-            rect(x * side, y * side, side, side);
+
 
         }
     }
@@ -40,26 +102,26 @@ function nkarel(matrix) {
 socket.on('send matrix', nkarel);
 
 
-function addGrass(){
+function addGrass() {
     socket.emit("add Grass");
 }
 
-function addGrassEater(){
+function addGrassEater() {
     socket.emit("add grassEater");
 }
 
-function addPredator(){
+function addPredator() {
     socket.emit("add Predator");
 }
 
-function addTrap(){
+function addTrap() {
     socket.emit("add Trap");
 }
 
-function addVampire(){
+function addVampire() {
     socket.emit("add Vampire");
 }
 
-function kill(){
+function kill() {
     socket.emit("kill");
 }
